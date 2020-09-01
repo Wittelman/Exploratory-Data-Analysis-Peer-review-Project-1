03e4ff5e3b2a2cc71c36a2001ba408e7f289f23d
Wittelman <66440589+Wittelman@users.noreply.github.com>
2020-09-01 11:40:28 -0500

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,8 @@
+#Plot 1
+power <- read.csv(".household_power_consumption.txt",sep=";")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+# calling the basic hist function
+png("Plot1.png", width=480, height=480)
+hist(as.numeric(subpower$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.off
